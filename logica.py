2faed7b932649a935c69444656f3b8f0b05f8a38
wilberson28 <110622550+wilberson28@users.noreply.github.com>
2022-08-29 18:52:10 -0500

    Create logica.py

---
+++ logica.py
@@ -0,0 +1,107 @@
+'''
+Librería con las clases y funciones
+para lógica proposicional
+'''
+
+from itertools import product
+
+class Formula :
+
+    def __init__(self) :
+        pass
+
+    def __str__(self) :
+        if type(self) == Letra:
+            return self.letra
+        elif type(self) == Negacion:
+            return '-' + str(self.subf)
+        elif type(self) == Binario:
+            return "(" + str(self.left) + self.conectivo + str(self.right) + ")"
+
+    def letras(self):
+        if type(self) == Letra:
+            return set(self.letra)
+        elif type(self) == Negacion:
+            return self.subf.letras()
+        elif type(self) == Binario:
+            return self.left.letras().union(self.right.letras())
+
+    def subforms(self):
+        if type(self) == Letra:
+            return [str(self)]
+        elif type(self) == Negacion:
+            return list(set([str(self)] + self.subf.subforms()))
+        elif type(self) == Binario:
+            return list(set([str(self)] + self.left.subforms() + self.right.subforms()))
+
+    def valor(self, I) :
+        if type(self) == Letra:
+            return I[self.letra]
+        elif type(self) == Negacion:
+            return not self.subf.valor(I)
+        elif type(self) == Binario:
+            if self.conectivo == 'Y':
+                return self.left.valor(I) and self.right.valor(I)
+            if self.conectivo == 'O':
+                return self.left.valor(I) or self.right.valor(I)
+            if self.conectivo == '>':
+                return not self.left.valor(I) or self.right.valor(I)
+            if self.conectivo == '=':
+                return (self.left.valor(I) and self.right.valor(I)) or (not self.left.valor(I) and not self.right.valor(I))
+
+class Letra(Formula) :
+    def __init__ (self, letra:str) :
+        self.letra = letra
+
+class Negacion(Formula) :
+    def __init__(self, subf:Formula) :
+        self.subf = subf
+
+class Binario(Formula) :
+    def __init__(self, conectivo:str, left:Formula, right:Formula) :
+        assert(conectivo in ['Y','O','>','='])
+        self.conectivo = conectivo
+        self.left = left
+        self.right = right
+
+def inorder_to_tree(cadena:str):
+    conectivos = ['Y', 'O', '>', '=']
+    if len(cadena) == 1:
+        return Letra(cadena)
+    elif cadena[0] == '-':
+        return Negacion(inorder_to_tree(cadena[1:]))
+    elif cadena[0] == "(":
+        counter = 0 #Contador de parentesis
+        for i in range(1, len(cadena)):
+            if cadena[i] == "(":
+                counter += 1
+            elif cadena[i] == ")":
+                counter -=1
+            elif cadena[i] in conectivos and counter == 0:
+                return Binario(cadena[i], inorder_to_tree(cadena[1:i]),inorder_to_tree(cadena[i + 1:-1]))
+    else:
+        raise Exception('¡Cadena inválida!')
+
+        
+def num_letras(self):
+        if type(self)==Letra:
+            return 1
+        elif type(self) == Negacion:
+            return 0 + self.subf.num_letras()
+        elif type(self) == Binario:
+            return 0 + self.left.num_letras() + self.right.num_letras()
+
+        
+def interpretaciones(self):    
+    interpretaciones=list(product([True, False], repeat=self.num_letras()))
+    print(interpretaciones)
+
+    interpretaciones_dict = []
+    for i in range(interpretaciones):
+        dict = {"p": True}
+        
+    
+
+    
+def chequear_equivalencia(fromula1, formula 2):
+    
